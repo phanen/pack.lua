40dd6a1c78364183af247135c2a00bce48cb748d
turtleDev <saravjeetamansingh@gmail.com>
2024-11-29 18:15:39 +0530

    examples: fix minification

--- example/minified/example.lua
+++ example/minified/example.lua
@@ -1,6 +1,6 @@
 -- An example of using modules similar to other languages
 -- Yet able to package them all together for a single script
 
-local json = require "modules/json"
+local json = require "modules/json.lua"
 
 print(json.decode("{\"hello\": \"World!\"}"))
